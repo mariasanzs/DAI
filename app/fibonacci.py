0e12339f11185360788161933aadebc909884c0e
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-10-21 21:22:41 +0200

    Subiendo sucesión Fibonacci :pencil:, #1

---
+++ app/fibonacci.py
@@ -0,0 +1,19 @@
+
+def fib(n):
+    if n < 2:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+def obtenerNumeroFichero():
+    n = open("entero.txt", "r")
+    numero = int(n.read())
+    n.close()
+    return numero
+
+def escribirFichero(numero):
+    salida = open("sucesionFibonacci_entero.txt", "w")
+    salida.write('SUCESIÓN DE FIBONACCI: \n')
+    salida.write(str(fib(numero)))
+    salida.write('\n')
+    salida.close()
