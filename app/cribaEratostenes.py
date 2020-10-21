262ba572f36794633b86eb37e401a714f5ebb25c
Maria Sanz <mariasanzsanchez45@gmail.com>
2020-10-21 21:20:27 +0200

    Subiendo criba Eratostenes :pencil:, #1

---
+++ app/cribaEratostenes.py
@@ -0,0 +1,19 @@
+
+#max = int(input("Introduzca hasta que número quieres buscar primos: "))
+
+def criba(max):
+    numeros = set ()
+    primos = ''
+
+    for i in range(2,max):
+        if i not in numeros:
+            primos += str(i) + ' , '
+            numeros.update(range(i*i, max, i))
+
+    return primos
+
+def imprimirLista(primos):
+    print("LISTADO DE PRIMOS:\n")
+    for i in range(len(primos)):
+        print(primos[i], ' ')
+    print('\n')
